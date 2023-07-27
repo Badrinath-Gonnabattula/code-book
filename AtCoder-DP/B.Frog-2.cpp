49d809acb5c5c586aef3f8c77b1bd89225b82b3c
Badrinath <63955356+Badrinath-Gonnabattula@users.noreply.github.com>
2023-07-27 10:06:36 +0530

    Rectified code for this problem

--- AtCoder-DP/B.Frog-2.cpp
+++ AtCoder-DP/B.Frog-2.cpp
@@ -5,46 +5,37 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 int n,k;
-int arr[100100];
-int dp[100100];
+vector<int> arr,dp;
 
 int rec(int level){
-    if(level<0){
-        return 1e9;
-    }
+    if(level<0) return 1e9;
+    if(level==0) return 0;
 
-    if(dp[level]!=-1){
-        return dp[level];   
-    }
+    if(dp[level]!=-1) return dp[level];
 
     int ans = 1e9;
-
     for(int i=1;i<=k;i++){
-        ans = min(ans,rec(level-i)+abs(arr[level] - arr[level-i]));
+        if(level>=i) ans = min(ans,rec(level-i)+abs(arr[level]-arr[level-i]));
     }
-
+    
     return dp[level] = ans;
 }
 
 void solve(){
     cin>>n>>k;
-
+    arr.clear();
     for(int i=0;i<n;i++){
-        cin>>arr[i];
-        dp[i] = -1;
+        int a;cin>>a;
+        arr.push_back(a);
     }
-
-    dp[0] = 0;
-
+    dp.clear();dp.assign(n+1,-1);
     cout<<rec(n-1)<<"\n";
-
 }
 
 signed main(){
     fast
 
     int t=1;
-    // cin>>t;
 
     while(t--){
         solve();
