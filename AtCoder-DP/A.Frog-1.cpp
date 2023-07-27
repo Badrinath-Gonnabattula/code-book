50ade2c894bbccdd06661ad7fc8b4c3331917d5b
Badrinath <63955356+Badrinath-Gonnabattula@users.noreply.github.com>
2023-07-27 09:32:43 +0530

    Updated with standard DP recursion coding style

--- AtCoder-DP/A.Frog-1.cpp
+++ AtCoder-DP/A.Frog-1.cpp
@@ -5,33 +5,28 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 int n;
-int arr[100100];
-int dp[100100];
+vector<int> arr,dp;
 
 int rec(int level){
-    if(level<0){
-        return 0;
-    }
+    if(level<1) return 0;
+    if(level==1) return abs(arr[level]-arr[level-1]);
 
-    if(dp[level]!=-1){
-        return dp[level];
-    }
+    if(dp[level]!=-1) return dp[level];
 
-    int ans = min(rec(level-1)+abs(arr[level-1]-arr[level]),rec(level-2)+abs(arr[level-2]-arr[level]));
+    int ans = abs(arr[level]-arr[level-1])+rec(level-1);
+    ans = min(ans,abs(arr[level]-arr[level-2])+rec(level-2));
 
     return dp[level] = ans;
 }
 
 void solve(){
     cin>>n;
-
     for(int i=0;i<n;i++){
-        cin>>arr[i];
-        dp[i] = -1;
+        int a;
+        cin>>a;
+        arr.push_back(a);
     }
-    
-    dp[0] = 0;
-    dp[1] = abs(arr[1] - arr[0]);
+    dp.clear();dp.assign(n+1,-1);
 
     cout<<rec(n-1)<<"\n";
 }
